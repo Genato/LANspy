91a4cff02aaefa7410eda16bc93ba343ab25329d
Renato.Katalenic <druidhr@gmail.com>
2017-02-12 02:25:07 +0100

    Setting up new MFC project.

---
+++ LANspy/LANspyDlg.h
@@ -0,0 +1,36 @@
+
+// LANspyDlg.h : header file
+//
+
+#pragma once
+
+
+// CLANspyDlg dialog
+class CLANspyDlg : public CDialogEx
+{
+// Construction
+public:
+	CLANspyDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LANSPY_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+};
